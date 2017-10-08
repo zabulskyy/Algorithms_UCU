c707aacc965ac0faa06794ffdf46312a0bcf8abb
vzabulskyy <v.zabulskyy@gmail.com>
2017-10-08 17:47:39 +0300

    merge sort task 2

--- task_02.py
+++ task_02.py
@@ -1,8 +1,9 @@
 def count_inversions(data, x):
-    arr_to_compare = data[x]
+    arr_to_compare = data[x]    # we will compare (sort by key of x) this array to others
 
     inversions = []
     for i in range(len(data)):
+        # inversions will be written here
         inversions += [[0, 0]]
 
     for i in range(len(data)):
@@ -14,16 +15,53 @@ def count_inversions(data, x):
         inversions[i][0] = i
         inversions[i][1] = count_inversions_in_single_array(data[i])
 
-    inversions.remove([x,0])
-    inversions.sort(key = lambda a: a[1])
-
-
+    inversions.remove([x,0])  # remove inverses of x with itself
+    inversions.sort(key = lambda a: a[1])  # sort by 2nd parameter
 
     return inversions
 
 
+def merge_sort(input):
+    """
+    actual merge sort
+    """
+    if len(input) == 2:
+        return input if input[0] < input[1] else input[::-1]
+    if len(input) == 1:
+        return input
+
+    l = len(input) // 2
+    return merge_dac(sort_dac(input[:l]), sort_dac(input[l:]))
+
+
+def merge(input1, input2):
+    """
+    merge two sorted arrays into one sorted array
+    """
+    input = []
+    for i in range(len(input1) + len(input2)):
+        if not input1:
+            input += input2
+            return input
+        if not input2:
+            input += input1
+            return input
+        if input1[0] < input2[0]:
+            input.append(input1[0])
+            input1 = input1[1:]
+        else:
+            input.append(input2[0])
+            input2 = input2[1:]
+    return input
+
 
 def revert_array(arr_to_compare, arr_to_revert):
+    """
+    sort arr_to_revert by key of arr_to_compare, for example:
+    arr_to_compare = [1, 0, 2]
+    arr_to_revert = [1, 2, 3]
+    return : [2, 1, 3]
+    """
     arr_to_compare = [x - 1 for x in arr_to_compare]
     new_arr = [0] * len(arr_to_revert)
 
@@ -37,6 +75,10 @@ def revert_array(arr_to_compare, arr_to_revert):
 
 
 def count_inversions_in_single_array(input):
+    """
+    we do actual 'sorting' and counting how many steps do we need to sort
+    this array, thus we get number of inversions
+    """
     n = 0
     for i in range(len(input)):
         j = i
@@ -47,7 +89,8 @@ def count_inversions_in_single_array(input):
             j -= 1
 
     return n
-    
+
+
 
 print(count_inversions([[3, 2, 10, 6, 9, 1, 5, 7, 4, 8],
                         [3, 2, 10, 6, 9, 1, 5, 7, 4, 8],

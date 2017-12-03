e0f7044513d4233c37ecb10fdc16b2b36c3dfc5c
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2017-12-03 17:01:02 +0200

    task_07 try 1.0

--- task_07.py
+++ task_07.py
@@ -4,25 +4,54 @@ def knapsack(items, weight):
     :param weight: int
     :return:  total_value, result_items
     """
+
+    # initialize local variables
     di = dict()
-    keys = list()
-    for item in items:
-        v = item[0]
-        w = item[1]
+    keys = set()  # keys of di, must be assigned as dict cannot be dynamically changed during iteration
+    k = set()
+
+    # iterate items
+    for item in range(len(items)):
+        v = items[item][0]  # value of current item
+        w = items[item][1]  # weight of current item
+
+        # iterating current keys
         for i in keys:
+            k = set()
+            # if we can't put i element and item element together
             if i + w > weight:
                 continue
-            else:
-                if i + w not in di:
-                    di[i + w] = (v + di[i][0], i)
-                    keys.append(i + w)
-                elif di[i + w][0] < v + di[i][0]:
-                    di[i + w] = (v + di[i][0], i)
-                    keys.append(i + w)
-        keys.append(w)
-        di[w] = (v, -1)
+
+            # if i + w is currently zero
+            if i + w not in di:
+                di[i + w] = (v + di[i][0], i, item)
+                k.add(i + w)
+
+            # if i + w is less than current v + di[i]
+            elif di[i + w][0] < v + di[i][0]:
+                di[i + w] = (v + di[i][0], i, item)
+                k.add(i + w)
+
+        # update keys
+        keys.add(w)
+        keys = keys.union(k)
+
+        # add current item to itself's cell
+        di[w] = (v, -1, item)
 
     print(di)
 
+    # finding max value and its path
+    current = di[max(di, key=lambda a: di[a][0])]
+    total_value = current[0]
+    path = list()
+    while 1:
+        path.append(current[2])
+        i = current[1]
+        if i == -1:
+            break
+        current = di[i]
+    return total_value, path
 
-knapsack([(7, 5), (8, 4), (1, 6)], 12)
+
+print(knapsack([(7, 5), (8, 4), (1, 6)], 12))
